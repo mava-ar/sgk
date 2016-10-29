7a38f2a8b8f37ca57c493fa3bd0f8f4101cf8efe
matuu <matu.varela@gmail.com>
2016-10-29 09:08:27 -0300

    Añado soporte para JWT

--- src/sgk/urls.py
+++ src/sgk/urls.py
@@ -18,15 +18,18 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-
-
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('dj_auth.urls')),
     url(r'^', include('frontend.urls')),
     url(r'^api/', include('api.urls')),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
