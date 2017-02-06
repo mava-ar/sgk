7b0f15c672bc26e56a73684bfb17753b5f0c01b4
matuu <matu.varela@gmail.com>
2017-02-06 13:14:20 -0300

    fix context processors with no logged users

--- src/tratamientos/context_processors.py
+++ src/tratamientos/context_processors.py
@@ -3,20 +3,22 @@ from tratamientos.models import Sesion
 
 
 def sesiones_activas(request):
-    try:
-        sesiones = Sesion.objects.filter(
-            profesional=request.user.profesional,
-            fin_el__isnull=True).select_related('motivo_consulta', 'paciente__persona')
-        data = []
-        for sesion in sesiones:
-            data.append({
-                'paciente_nombre': str(sesion.paciente.persona),
-                'comienzo_el': sesion.comienzo_el,
-                'duracion': sesion.duracion,
-                'persona': sesion.paciente.persona,
-                'paciente_pk': sesion.paciente_id,
-                'motivo_pk': sesion.motivo_consulta_id
-            })
-        return {'sesiones_activas': data}
-    except Profesional.DoesNotExist:
-        return {}
+    if request.user.is_authenticated():
+        try:
+            sesiones = Sesion.objects.filter(
+                profesional=request.user.profesional,
+                fin_el__isnull=True).select_related('motivo_consulta', 'paciente__persona')
+            data = []
+            for sesion in sesiones:
+                data.append({
+                    'paciente_nombre': str(sesion.paciente.persona),
+                    'comienzo_el': sesion.comienzo_el,
+                    'duracion': sesion.duracion,
+                    'persona': sesion.paciente.persona,
+                    'paciente_pk': sesion.paciente_id,
+                    'motivo_pk': sesion.motivo_consulta_id
+                })
+            return {'sesiones_activas': data}
+        except Profesional.DoesNotExist:
+            pass
+    return {}
